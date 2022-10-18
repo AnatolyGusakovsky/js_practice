4ed41570e4f3edc09fac6916b0b4aba432ddf8b2
Anatoly <anatoly.gusakovsky@gmail.com>
2022-10-18 09:24:01 +0300

    digital_root - done

--- codewars/algorithms/6_kyu/sum_of_digits_or_digital_root.js
+++ codewars/algorithms/6_kyu/sum_of_digits_or_digital_root.js
@@ -8,21 +8,41 @@
 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2*/
 
-// todo: in progress
+
+// NOTE! for real-life needs there is much simpler/faster/better solution for this task. I've done this as I can, without googling, so solution is not ideal)
+
+
+function digitalRoot(n) {
+  console.log('n: ' + n)
+  let result = n;
+  while (get_number_of_digits(result) > 1) {
+    result = get_sum_of_digits(number_to_array_of_digits(result))
+  }
+  return result;
+}
+
 function get_number_of_digits(number) {
-  // number = Math.abs(number)
   let number_of_digits = 0
-  let numbers = []
-  do {
-    numbers.push(number%10)
-    number = number / 10
-    if (number === 1)
-      number_of_digits++
+  while (number >= 1) {
+    number = Math.trunc(number / 10)
     number_of_digits++
   }
-  while (number > 1)
-console.log('numbers: ' + numbers)
   return number_of_digits;
 }
 
-console.log(get_number_of_digits(999))
+function number_to_array_of_digits(n) {
+  let numbers = []
+  while (n >= 1) {
+    numbers.push(n % 10)
+    n = Math.trunc(n / 10)
+  }
+  return numbers.reverse()
+}
+
+function get_sum_of_digits(array) {
+  let res = 0;
+  array.forEach((digit) => {
+    res += digit;
+  })
+  return res;
+}
