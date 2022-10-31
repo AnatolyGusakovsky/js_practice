cd174ab27654724cea7874823997bdbc8355e8a6
Anatoly <anatoly.gusakovsky@gmail.com>
2022-10-31 09:35:29 +0200

    4_kyu algorithmic task - permutations [in progress 4] Algorithm has been found

--- codewars/algorithms/4_kyu/permutations.js
+++ codewars/algorithms/4_kyu/permutations.js
@@ -11,7 +11,8 @@ In this kata you have to create all permutations of a non empty input string and
 * Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
 The order of the permutations doesn't matter.*/
 
-//todo: 1. create algorithm first
+// algorithm has been created.
+//todo: 1. decompose on steps, 2. implement
 function permutations(string) {
   let result_arr, string_digits_arr;
   string_digits_arr = string.split('');
@@ -34,4 +35,9 @@ function get_factorial(n){
     return 1
   return n*get_factorial(n-1)
 }
+
+// Thinking on algorithm:
+// 1. Count of symbols in given string = number of calculation blocks
+// 2. Create array with blocks by rearranging each symbol in string on first place (doesn't matter, switch places with current first symbol or just move on first place)
+// 3. Start permute last 2 symbols, then remember this 2 combinations and for each permute with 3th symbol from the end and do it till the first symbol recursively
 
