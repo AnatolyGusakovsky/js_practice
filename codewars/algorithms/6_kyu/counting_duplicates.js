2fa7036fc93a10d5186ac0945edffddffec5fb59
Anatoly <anatoly.gusakovsky@gmail.com>
2022-11-30 08:36:53 +0200

    6_kyu algorithmic task - counting_duplicates.js - done

--- codewars/algorithms/6_kyu/counting_duplicates.js
+++ codewars/algorithms/6_kyu/counting_duplicates.js
@@ -1,6 +1,8 @@
 /*
 Count the number of Duplicates
-Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+Write a function that will return the count of distinct case-insensitive alphabetic characters
+and numeric digits that occur more than once in the input string.
+The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 
   Example
 "abcde" -> 0 # no characters repeats more than once
@@ -11,6 +13,29 @@ Write a function that will return the count of distinct case-insensitive alphabe
 "aA11" -> 2 # 'a' and '1'
 "ABBA" -> 2 # 'A' and 'B' each occur twice*/
 
-function duplicateCount(text){
-  //...
+function duplicateCount(text) {
+  let given_chars = text.toLowerCase().split('')
+  let char_set = new Set(given_chars)
+
+  if (char_set.size === given_chars.length)
+    return 0
+
+  let entry_map = new Map();
+  char_set.forEach((elem) => {
+    let counter = 0
+    given_chars.forEach((c) => {
+      if (elem === c)
+        counter++
+    })
+    entry_map.set(elem, counter)
+  })
+
+  let result_counter = 0;
+  entry_map.forEach((value) => {
+    if (value >= 2)
+      result_counter++
+  })
+  return result_counter
 }
+
+console.log(duplicateCount("Indivisibilities"))
